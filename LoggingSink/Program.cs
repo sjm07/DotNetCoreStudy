40a83f0260f13547cd5386654f424e19c6e44e73
wangmj <501305513@qq.com>
2019-03-08 15:15:10 +0800

    init commit

---
+++ LoggingSink/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggingSink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
